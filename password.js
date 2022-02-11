a72764cba8f261b31d3da33e2fd03315675cb03e
Sefton Dockstader <sefdock@gmail.com>
2022-02-11 10:45:25 -0800

    Solve password problem

--- password.js
+++ password.js
@@ -1,5 +1,58 @@
-
-console.log("Welcome to the password validator tool!")
+var format = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/;
+console.log(` __       __            __                                             
+/  |  _  /  |          /  |                                            
+$$ | / \ $$ |  ______  $$ |  _______   ______   _____  ____    ______  
+$$ |/$  \$$ | /      \ $$ | /       | /      \ /     \/    \  /      \ 
+$$ /$$$  $$ |/$$$$$$  |$$ |/$$$$$$$/ /$$$$$$  |$$$$$$ $$$$  |/$$$$$$  |
+$$ $$/$$ $$ |$$    $$ |$$ |$$ |      $$ |  $$ |$$ | $$ | $$ |$$    $$ |
+$$$$/  $$$$ |$$$$$$$$/ $$ |$$ \_____ $$ \__$$ |$$ | $$ | $$ |$$$$$$$$/ 
+$$$/    $$$ |$$       |$$ |$$       |$$    $$/ $$ | $$ | $$ |$$       |
+$$/      $$/  $$$$$$$/ $$/  $$$$$$$/  $$$$$$/  $$/  $$/  $$/  $$$$$$$/ 
+                                                                       `)
+                                                                       
+console.log(`   __                        __      __                 
+/  |                      /  |    /  |                
+_$$ |_     ______         _$$ |_   $$ |____    ______  
+/ $$   |   /      \       / $$   |  $$      \  /      \ 
+$$$$$$/   /$$$$$$  |      $$$$$$/   $$$$$$$  |/$$$$$$  |
+$$ | __ $$ |  $$ |        $$ | __ $$ |  $$ |$$    $$ |
+$$ |/  |$$ \__$$ |        $$ |/  |$$ |  $$ |$$$$$$$$/ 
+$$  $$/ $$    $$/         $$  $$/ $$ |  $$ |$$       |
+ $$$$/   $$$$$$/           $$$$/  $$/   $$/  $$$$$$$/ `)
+console.log(`                                                                                __ 
+/  |
+______    ______    _______  _______  __   __   __   ______    ______    ____$$ |
+/      \  /      \  /       |/       |/  | /  | /  | /      \  /      \  /    $$ |
+/$$$$$$  | $$$$$$  |/$$$$$$$//$$$$$$$/ $$ | $$ | $$ |/$$$$$$  |/$$$$$$  |/$$$$$$$ |
+$$ |  $$ | /    $$ |$$      \$$      \ $$ | $$ | $$ |$$ |  $$ |$$ |  $$/ $$ |  $$ |
+$$ |__$$ |/$$$$$$$ | $$$$$$  |$$$$$$  |$$ \_$$ \_$$ |$$ \__$$ |$$ |      $$ \__$$ |
+$$    $$/ $$    $$ |/     $$//     $$/ $$   $$   $$/ $$    $$/ $$ |      $$    $$ |
+$$$$$$$/   $$$$$$$/ $$$$$$$/ $$$$$$$/   $$$$$/$$$$/   $$$$$$/  $$/        $$$$$$$/ 
+$$ |                                                                               
+$$ |                                                                               
+$$/                                                                                `)
+console.log(`                     __  __        __              __                         
+/  |/  |      /  |            /  |                        
+__     __  ______  $$ |$$/   ____$$ |  ______   _$$ |_     ______    ______  
+/  \   /  |/      \ $$ |/  | /    $$ | /      \ / $$   |   /      \  /      \ 
+$$  \ /$$/ $$$$$$  |$$ |$$ |/$$$$$$$ | $$$$$$  |$$$$$$/   /$$$$$$  |/$$$$$$  |
+$$  /$$/  /    $$ |$$ |$$ |$$ |  $$ | /    $$ |  $$ | __ $$ |  $$ |$$ |  $$/ 
+$$ $$/  /$$$$$$$ |$$ |$$ |$$ \__$$ |/$$$$$$$ |  $$ |/  |$$ \__$$ |$$ |      
+$$$/   $$    $$ |$$ |$$ |$$    $$ |$$    $$ |  $$  $$/ $$    $$/ $$ |      
+$/     $$$$$$$/ $$/ $$/  $$$$$$$/  $$$$$$$/    $$$$/   $$$$$$/  $$/       
+                                                          `)
+console.log(`   __                          __  __ 
+/  |                        /  |/  |
+_$$ |_     ______    ______  $$ |$$ |
+/ $$   |   /      \  /      \ $$ |$$ |
+$$$$$$/   /$$$$$$  |/$$$$$$  |$$ |$$ |
+$$ | __ $$ |  $$ |$$ |  $$ |$$ |$$/ 
+$$ |/  |$$ \__$$ |$$ \__$$ |$$ | __ 
+$$  $$/ $$    $$/ $$    $$/ $$ |/  |
+ $$$$/   $$$$$$/   $$$$$$/  $$/ $$/ 
+                                    
+                                    
+                                    `)
 const readline = require("readline");
 
 const reader = readline.createInterface({
@@ -7,11 +60,21 @@ const reader = readline.createInterface({
   output: process.stdout,
 });
 
-reader.question("Please enter a password:", function (password) {
+    reader.question("Please enter a password:", function (password) {
   if(password.length < 10){
-      console.log("Attempt failed! Password must be more than 10 characters long!")
-  } else {
-      console.log("Attempt successful!")
+      console.log("Attempt failed! Password must be at least 10 characters long!")
+  } else if(stringHasNum(password) === false){
+      console.log("Attempt failed! Password must contain a number!")
+  } else if (format.test(password) === false){
+      console.log(`Attempt failed! Password must contain special character: /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/</>`)
+  }else {
+      console.log("Attempt Successful!")
+      validInput = true;
   }
   reader.close();
 });
+
+//function to check if a string has a number in it
+function stringHasNum( inputString ){
+    return /\d/.test( inputString );
+  }
