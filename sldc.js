cba5040754d35830b6bfcff99dcf64ef9ee6c471
Sefton Dockstader <sefdock@gmail.com>
2022-02-11 09:12:25 -0800

    Answer sldc questions

--- sldc.js
+++ sldc.js
@@ -5,12 +5,12 @@
     Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
 */
 
+// Planning
 // Analysis of Requirements
 // Design
 // Implementation
-// Maintain
-// Planning
 // Testing & Integration
+// Maintain
 
 
 //////////////////PROBLEM 2////////////////////
@@ -19,3 +19,8 @@
     The first one is done for you. 
 */
 const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+const analysis = "This is the second step. In this step, senior members of a development team will gather information about the requirements of the software from the customer, put it into writing and have the customer approve the list of requirements."
+const design = "This is the third step. In this step the development team comes up with a solution to the request of the customer and outlines how the requirements will be met."
+const implimentation = "This is the fourth step. In this step the team works on building the software by implimenting the design created in step three."
+const testing = "This is the fifth step. In this step, the application is put through rigorous trials to discover any flaws in it. The bugs are then reported, tracked and repaired."
+const maintenance = "This is the sixth step. In this step, the product is deployed to the customer. Any desired changes are then reported and the product is updated to reflect the changes."
